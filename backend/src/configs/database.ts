b788b91957455217b5798f8c1454b906c478f2b0
vslipchenko <holyhostell@gmail.com>
2025-08-18 17:30:54 +0200

    backend: add database config

---
+++ backend/src/configs/database.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import { Product } from '../entities/Product';
+
+export const AppDataSource = new DataSource({
+  type: 'mariadb',
+  host: process.env['DB_HOST'] || 'localhost',
+  port: parseInt(process.env['DB_PORT'] || '3306'),
+  username: process.env['DB_USERNAME'] || 'warehouse',
+  password: process.env['DB_PASSWORD'] || 'warehousepassword',
+  database: process.env['DB_DATABASE'] || 'warehouse',
+  synchronize: process.env['NODE_ENV'] !== 'production',
+  logging: process.env['NODE_ENV'] === 'development',
+  entities: [Product],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscribers/**/*.ts'],
+});
+
