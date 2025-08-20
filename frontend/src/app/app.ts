0a519ccba9f734b2a176d1549ad4f4cd352e6afa
vslipchenko <holyhostell@gmail.com>
2025-08-20 15:42:28 +0200

    frontend: update app component to integrate new components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar';
+import { TableComponent } from './components/table/table';
+import { FooterComponent } from './components/footer/footer';
+import { HeadlineComponent } from './components/headline/headline';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [
+    ToolbarComponent,
+    TableComponent,
+    FooterComponent,
+    HeadlineComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'warehouse';
-}
+export class AppComponent {}
