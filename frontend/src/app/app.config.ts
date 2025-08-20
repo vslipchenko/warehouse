8aaec17eb78a857d6a00617c3d968cc36eb859e8
vslipchenko <holyhostell@gmail.com>
2025-08-20 14:10:51 +0200

    frontend: configure PrimeNG theme

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
+  ],
+};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent).catch((error) => console.error(error));
+bootstrapApplication(AppComponent, appConfig).catch((error) =>
+  console.error(error)
+);
