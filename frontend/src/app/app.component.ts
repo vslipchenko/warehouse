18bad344ff77d6ae12c5b2c7f167ba137c7bec53
vslipchenko <holyhostell@gmail.com>
2025-08-20 14:11:59 +0200

    frontend: add toolbar panel

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Toolbar} from "primeng/toolbar";
+import {Button} from "primeng/button";
+import {Avatar} from "primeng/avatar";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [RouterOutlet, Toolbar],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'warehouse';
