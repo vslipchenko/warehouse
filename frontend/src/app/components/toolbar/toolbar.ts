e6d03a0e7dc7841b7b22936292d1afd983d14c87
vslipchenko <holyhostell@gmail.com>
2025-08-20 14:26:16 +0200

    frontend: move toolbar logic to a separate component; rename App class

---
+++ frontend/src/app/components/toolbar/toolbar.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Toolbar } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [Toolbar],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app';
+import { config } from './app/config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((error) =>
+bootstrapApplication(AppComponent, config).catch((error) =>
   console.error(error)
 );
