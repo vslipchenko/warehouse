3496ff67a00a237c808ba6101b74b58b0845d54a
vslipchenko <holyhostell@gmail.com>
2025-08-21 13:08:31 +0200

    frontend: add products load logic and loading state

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import { ToolbarComponent } from './components/toolbar/toolbar';
 import { TableComponent } from './components/table/table';
 import { FooterComponent } from './components/footer/footer';
 import { HeadlineComponent } from './components/headline/headline';
 import { AddProductDialogComponent } from './components/add-product-dialog/add-product-dialog';
+import { GraphqlService } from './services/graphql.service';
+import { catchError, of, tap } from 'rxjs';
+import { Product } from './models/product';
+import { ProgressSpinner } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +24,45 @@ import { AddProductDialogComponent } from './components/add-product-dialog/add-p
     FooterComponent,
     HeadlineComponent,
     AddProductDialogComponent,
+    ProgressSpinner,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  protected readonly loading = signal(false);
   protected readonly showAddProductDialog = signal(false);
+  protected readonly products = signal<Array<Product>>([]);
+  private readonly graphqlService = inject(GraphqlService);
+
+  public constructor() {
+    this.loadProducts();
+  }
 
   protected openAddProductDialog(): void {
     this.showAddProductDialog.set(true);
+  }
+
+  private loadProducts(): void {
+    this.loading.set(true);
+
+    this.graphqlService
+      .getProducts()
+      .pipe(
+        tap(() => {
+          this.loading.set(false);
+        }),
+        catchError((error) => {
+          console.error('Error loading products:', error);
+
+          return of({ data: { products: [] } });
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          if (!response?.data?.products) return;
+
+          this.products.set(response.data.products);
+        },
+      });
   }
 }
