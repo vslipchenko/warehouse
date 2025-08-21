c6c012b6d738f22bbee0b491bf65582421d5ee19
vslipchenko <holyhostell@gmail.com>
2025-08-21 15:21:46 +0200

    frontend: add action buttons to the table component

--- frontend/src/app/components/table/table.ts
+++ frontend/src/app/components/table/table.ts
@@ -1,14 +1,23 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { Product } from '../../models/product';
+import { Button } from 'primeng/button';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-table',
-  imports: [TableModule],
+  imports: [TableModule, Button, Tooltip],
   templateUrl: './table.html',
   styleUrl: './table.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableComponent {
   public readonly products = input.required<Array<Product>>();
+  public readonly editProduct = output<void>();
+  public readonly deleteProduct = output<void>();
 }
