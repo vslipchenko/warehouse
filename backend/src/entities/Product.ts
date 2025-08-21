f9ed0cc333b90212a667c7c5a2a2bc2cb360b934
vslipchenko <holyhostell@gmail.com>
2025-08-21 11:40:07 +0200

    backend: make use of constants

--- backend/src/entities/Product.ts
+++ backend/src/entities/Product.ts
@@ -1,6 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import { ObjectType, Field, ID, Int, Float } from 'type-graphql';
-import { IsNotEmpty, IsPositive, Min, MaxLength } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Field, Float, ID, Int, ObjectType } from 'type-graphql';
+import { IsNotEmpty, IsPositive, MaxLength, Min } from 'class-validator';
+import {
+  PRODUCT_DESCRIPTION_MAX_LENGTH,
+  PRODUCT_NAME_MAX_LENGTH,
+} from '../constants/product';
 
 @ObjectType()
 @Entity('products')
@@ -10,25 +20,28 @@ export class Product {
   id!: number;
 
   @Field()
-  @Column({ length: 255 })
+  @Column({ length: PRODUCT_NAME_MAX_LENGTH })
   @IsNotEmpty()
-  @MaxLength(255)
+  @MaxLength(PRODUCT_NAME_MAX_LENGTH)
   name!: string;
 
   @Field(() => Int)
   @Column('int')
+  @IsNotEmpty()
   @IsPositive()
   @Min(0)
   quantity!: number;
 
   @Field(() => Float)
   @Column('decimal', { precision: 10, scale: 2 })
+  @IsNotEmpty()
   @IsPositive()
+  @Min(0)
   unitPrice!: number;
 
   @Field({ nullable: true })
-  @Column({ length: 1000, nullable: true })
-  @MaxLength(1000)
+  @Column({ length: PRODUCT_DESCRIPTION_MAX_LENGTH, nullable: true })
+  @MaxLength(PRODUCT_DESCRIPTION_MAX_LENGTH)
   description?: string;
 
   // @Field({ nullable: true })
@@ -44,4 +57,3 @@ export class Product {
   @UpdateDateColumn()
   updatedAt!: Date;
 }
-
