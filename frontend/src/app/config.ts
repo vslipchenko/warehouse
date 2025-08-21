4cc53834b8c2f3f66f0e3665b6b93cd6b7c80480
vslipchenko <holyhostell@gmail.com>
2025-08-21 12:26:02 +0200

    frontend: update Apollo config

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -2,23 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeuix/themes/aura';
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from '@apollo/client/core';
+import { createHttpLink, InMemoryCache } from '@apollo/client/core';
 import { provideApollo } from 'apollo-angular';
 import { environment } from '../environments/environment';
-
-// Create the Apollo client
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql',
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
 
 export const config: ApplicationConfig = {
   providers: [
