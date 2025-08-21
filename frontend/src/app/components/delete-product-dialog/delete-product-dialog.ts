2a96296830514ab7e6bc30220d4900ee055b8ce5
vslipchenko <holyhostell@gmail.com>
2025-08-21 15:45:13 +0200

    frontend: implement product deletion logic

--- frontend/src/app/components/delete-product-dialog/delete-product-dialog.ts
+++ frontend/src/app/components/delete-product-dialog/delete-product-dialog.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   input,
   signal,
   WritableSignal,
@@ -8,18 +9,26 @@ import {
 import { Button } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 import { FormsModule } from '@angular/forms';
+import { GraphqlService } from '../../services/graphql.service';
+import { untilDestroyed } from '../../utilities/operator';
+import { catchError, finalize, of } from 'rxjs';
+import { Product } from '../../models/product';
+import { ErrorBannerComponent } from '../error-banner/error-banner';
 
 @Component({
   selector: 'app-delete-product-dialog',
-  imports: [Button, Dialog, FormsModule],
+  imports: [Button, Dialog, FormsModule, ErrorBannerComponent],
   templateUrl: './delete-product-dialog.html',
   styleUrl: './delete-product-dialog.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeleteProductDialogComponent {
   public readonly visible = input.required<WritableSignal<boolean>>();
-  public readonly productName = input.required<string>();
+  public readonly product = input.required<Product>();
   protected readonly loading = signal(false);
+  protected readonly error = signal(false);
+  private readonly untilDestroyed = untilDestroyed();
+  private readonly graphqlService = inject(GraphqlService);
 
   get show(): boolean {
     return this.visible()();
@@ -29,7 +38,31 @@ export class DeleteProductDialogComponent {
     this.visible().set(visible);
   }
 
-  protected deleteProduct(): void {
+  protected deleteProduct(productId: number): void {
+    this.error.set(false);
     this.loading.set(true);
+
+    this.graphqlService
+      .deleteProduct(productId)
+      .pipe(
+        this.untilDestroyed(),
+        catchError((error) => {
+          console.error('Error creating product:', error);
+
+          this.error.set(true);
+
+          return of(null);
+        }),
+        finalize(() => {
+          this.loading.set(false);
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          if (!response) return;
+
+          this.show = false;
+        },
+      });
   }
 }
